fd7e3d4c1f1ae0e0168fe48e4ea240b147a7bf45
zhengyongjie <867991328@qq.com>
2018-05-22 11:54:18 +0800

    add

---
+++ a.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+a=`grep 'zyj_gitLabel版本:*' ../../a`
+echo $a
+version=`echo $a|grep -o '[0-9]\+'`
+version=$[version+1]
+version=$((10#$version))
+#version=9
+version=$(printf "%04d" "$((version))")
+str2=" zyj_gitLabel版本:  "$version
+#echo $str2
+grep 'zyj_gitLabel版本:*' ../../a
+sed -ig "s/$a/$str2/"  ../../a
